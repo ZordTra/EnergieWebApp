ec60be24eb61c3dbdf25d72d956e9132e6ba7a3a
ZordTra <Caspervdp2a@gmail.com>
2023-10-27 10:47:46 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnergieWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
