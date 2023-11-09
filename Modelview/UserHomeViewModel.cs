11af95fc297c416396452332a8052fc1f7ebafed
al3n5s <2065272reinfelds@zuyd.nl>
2023-11-10 00:43:30 +0100

    scorebord werkend gemaakt

---
+++ Modelview/UserHomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EnergieWebApp.Models;
+
+namespace EnergieWebApp.Modelview
+{
+    public class UserHomeViewModel
+    {
+
+        public User User { get; set; }
+        public List<(String, double?)> Scores { get; set; }
+
+
+    }
+}
