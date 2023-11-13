30108885e8dd7054471552eec50930720b26018e
ZordTra <Caspervdp2a@gmail.com>
2023-11-13 01:19:39 +0100

    Edit houshold device

---
+++ Controllers/DayDatasApiController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EnergieWebApp.Data;
+using EnergieWebApp.Models;
+
+namespace EnergieWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DayDatasApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DayDatasApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DayDatasApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DayData>>> GetDayDatas()
+        {
+          if (_context.DayDatas == null)
+          {
+              return NotFound();
+          }
+            return await _context.DayDatas.ToListAsync();
+        }
+
+        // GET: api/DayDatasApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DayData>> GetDayData(int id)
+        {
+          if (_context.DayDatas == null)
+          {
+              return NotFound();
+          }
+            var dayData = await _context.DayDatas.FindAsync(id);
+
+            if (dayData == null)
+            {
+                return NotFound();
+            }
+
+            return dayData;
+        }
+
+        // PUT: api/DayDatasApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDayData(int id, DayData dayData)
+        {
+            if (id != dayData.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(dayData).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DayDataExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DayDatasApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DayData>> PostDayData(DayData dayData)
+        {
+          if (_context.DayDatas == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.DayDatas'  is null.");
+          }
+            _context.DayDatas.Add(dayData);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDayData", new { id = dayData.Id }, dayData);
+        }
+
+        // DELETE: api/DayDatasApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDayData(int id)
+        {
+            if (_context.DayDatas == null)
+            {
+                return NotFound();
+            }
+            var dayData = await _context.DayDatas.FindAsync(id);
+            if (dayData == null)
+            {
+                return NotFound();
+            }
+
+            _context.DayDatas.Remove(dayData);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DayDataExists(int id)
+        {
+            return (_context.DayDatas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ namespace EnergieWebApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
@@ -22,16 +27,19 @@ namespace EnergieWebApp
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (!app.Environment.IsDevelopment())
             {
-                app.UseMigrationsEndPoint();
+                //app.UseMigrationsEndPoint();
+                app.UseHsts();
+                app.UseExceptionHandler("/Home/Error");
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+
             }
 
             app.UseHttpsRedirection();
